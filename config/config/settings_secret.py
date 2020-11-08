22004b1b076eb5821ee59917002e79f8f3a15cdd
roughdiary <miyama.med@gmail.com>
2020-11-08 15:45:02 +0900

    third

---
+++ config/config/settings_secret.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'x#t@%56cb3+i6%mfv-sgb7+vwlw#eey47#r*9bscuxz37^90ci'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
